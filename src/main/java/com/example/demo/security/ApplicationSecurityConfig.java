ca6c2428f77b1a1c12b3830117abeaa8b15b061e
Ved Asole <vedasole001@gmail.com>
2021-09-18 10:10:05 +0530

    User Authentication with one role

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    // Setting up the local variable in the constructor
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +34,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    // Setting up the InMemory user details
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        String encodedPass=passwordEncoder.encode("password");
+        UserDetails markHunt = User.builder()
+                .username("markHunt")
+                .password(encodedPass)
+                .roles("STUDENT") // ROLE_STUDENT(Internal name - Given by Spring)
+                .build();
+        return new InMemoryUserDetailsManager(markHunt);
     }
 }
